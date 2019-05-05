a301d154546e2e7a75a1393bb47c55ff6f48ca81
Lucas Leandro <lucasleandro380@gmail.com>
2019-05-05 20:51:25 -0300

    * Fix: Carousel and Refactory views
    - Remove Carousel image for prevent (No SSL ) http errors
    - Refactory view pages

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
-
 // Import Tailwindcss
 import "@/assets/css/tailwind.css";
 
