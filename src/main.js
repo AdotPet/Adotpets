da119e0d31e2000b3234bfbc2bea7b729642881f
Lucas Leandro <lucasleandro380@gmail.com>
2019-07-05 17:45:19 -0300

    Fixes and login page

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import "./registerServiceWorker";
 import { VueSlideoutPanel } from "vue2-slideout-panel";
 
 Vue.component("slideout-panel", VueSlideoutPanel);
+
 // Import Tailwindcss
 import "@/assets/css/tailwind.css";
+
+import store from "./store";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
